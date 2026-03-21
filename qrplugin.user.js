fdcf42bcbece50e102188fdcbfaf31b8799c3ea7
LL <81279812+14111403448@users.noreply.github.com>
2026-03-21 10:39:23 +0800

    Update qrplugin.user.js

--- qrplugin.user.js
+++ qrplugin.user.js
@@ -7,8 +7,7 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @author       LCJ
-// @match        https://*.onedp.vip.vip.com/*
-// @match        https://*.pos-admin.vip.vip.com/*
+// @match        https://*.vip.vip.com/*
 // @icon         https://cdn-icons.flaticon.com/svg/3917033.svg
 // @require      https://cdn.jsdelivr.net/npm/qrcode@1.5.1/build/qrcode.min.js
 // @downloadURL  http://www.lcjscript.asia/qrplugin.user.js
