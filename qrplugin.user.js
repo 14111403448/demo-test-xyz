9fc41e80fa02cbaf0266100f36d8f81999243286
LL <81279812+14111403448@users.noreply.github.com>
2026-09-17 15:59:48 +0800

    Update download and update URLs in user script

--- qrplugin.user.js
+++ qrplugin.user.js
@@ -10,8 +10,8 @@
 // @match        https://*.vip.vip.com/*
 // @icon         https://cdn-icons.flaticon.com/svg/3917033.svg
 // @require      https://cdn.jsdelivr.net/npm/qrcode@1.5.1/build/qrcode.min.js
-// @downloadURL  https://www.lcjscript.asia/qrplugin.user.js
-// @updateURL    https://www.lcjscript.asia/qrplugin.user.js
+// @downloadURL  https://14111403448.github.io/demo-test-xyz/qrplugin.user.js
+// @updateURL    https://14111403448.github.io/demo-test-xyz/qrplugin.user.js
 // ==/UserScript==
 
 (function() {
